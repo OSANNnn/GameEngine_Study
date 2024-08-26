7c234a1d8c92c705305882afe3f05a02e78d863f
Roby Osan <osannnn@gmail.com>
2024-08-26 19:12:57 +0200

    added to repo premake and script for proj gen

---
+++ premake5.lua
@@ -0,0 +1,107 @@
+workspace "GameEngine_Study"
+	architecture "x64"
+	startproject "Sandbox"
+	
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Engine"
+	location "Engine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ENG_PLATFORM_WINDOWS",
+			"ENG_BUILD_DLL"
+		}
+
+	filter "configurations:Debug"
+		defines "ENG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ENG_Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ENG_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Engine/vendor/spdlog/include",
+		"Engine/src"
+	}
+
+	links
+	{
+		"Engine"
+	}
+
+	postbuildcommands 
+	{
+
+		"{COPYFILE} ../bin/" .. outputdir .. "/Engine/Engine.dll ../bin/" .. outputdir .. "/Sandbox"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ENG_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "ENG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ENG_Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ENG_DIST"
+		optimize "On"
