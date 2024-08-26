824dd661204f18bb01b163b390bfce6d11ead602
Roby Osan <osannnn@gmail.com>
2024-08-26 19:14:29 +0200

    added files to repo

--- Engine/src/Engine/EntryPoint.h
+++ Engine/src/Engine/EntryPoint.h
@@ -6,6 +6,11 @@ extern Engine::Application* Engine::CreateApplication();
 
 int main(const int argc, const char** argv)
 {
+	int x = 6; //delete this
+	Engine::Log::Init();
+	ENG_CORE_WARN("Init LOG");//delete this
+	ENG_TRACE("APP Log init var = {0}", x);//delete this
+
 	auto app = Engine::CreateApplication();
 	app->Run();
 	delete app;
